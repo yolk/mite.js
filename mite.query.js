113da72043a483e9182e58f156757317781e3cd0
Sebastian <sebastian@yo.lk>
2011-03-17 13:43:14 +0100

    window.miteQuery as simple function for real private and instanized vars (config)

--- mite.query.js
+++ mite.query.js
@@ -1,51 +1,45 @@
 (function(window) {  
-  var defaults  = { protocol : 'https'
-                  , domain   : 'mite.yo.lk'
-                  , account  : ''
-                  , api_key  : ''
-                  , async    : true
-                  , timeout  : 60 // 1 minute
-                  , error  : function(xhr, msg) {alert('Error: mite.gyver could not connect with your mite.account!');}
-                  };
-  
-  window.miteQuery = (function() {
+  var _defaults      = { 
+    protocol : 'https',
+    domain   : 'mite.yo.lk',
+    async    : true,
+    timeout  : 60, // 1 minute
+    error  : function(xhr, msg) {alert('Error: mite.gyver could not connect with your mite.account!');}
+  },
+      _nada         = function() {},
+      _parseJson    = function(string) { return ( /^\s*$/.test(string) ) ? {} : JSON.parse(string); },
+      _buildQuery   = function(json) {
+    if (!json) { return ""; }
+    
+    var params = [];
+    for(key in json) {
+      params.push([encodeURIComponent(key),encodeURIComponent(json[key])].join('='));
+    }
+    return params.join('&');
+  },
+      _parseOptions = function(options) {
+    if(typeof options == 'function') { options = {success: options}; }
+    return options || {};
+  },
+      _extend       = function(obj) {
+    for(var i = 1, len = arguments.length; i < len; i++ ) {
+      for (var prop in arguments[i]) { obj[prop] = arguments[i][prop]; }
+    }
+    return obj;
+  };
+
+  window.miteQuery = function(options) {
+    if (!options || !options.account || !options.api_key) {
+      throw "account & api_key need to be set";
+    }
     
     ////
     //  Private
-    var config = {}, 
-        nada   = function() {},
+    var config = _extend({}, _defaults, options),
     
     // build URL for API request
     _buildUrl = function(path) {
       return config.protocol + '://' + 'corsapi.' + config.domain + '/' + path + '.json';
-    },
-    
-    // parse string to JSON
-    _parseJson = function(string) {
-      return ( /^\s*$/.test(string) ) ? {} : JSON.parse(string);
-    },
-    
-    // build a query out of an associative array
-    _buildQuery = function(json) {
-      if (!json) { return ""; }
-      
-      var params = [];
-      for(key in json) {
-        params.push([encodeURIComponent(key),encodeURIComponent(json[key])].join('='));
-      }
-      return params.join('&');
-    },
-    
-    _parseOptions = function(options) {
-      if(typeof options == 'function') { options = {success: options}; }
-      return options || {};
-    },
-    
-    _extend = function(obj) {
-      for(var i = 1, len = arguments.length; i < len; i++ ) {
-        for (var prop in arguments[i]) { obj[prop] = arguments[i][prop]; }
-      }
-    	return obj;
     },
     
     // ajax call wrapper
@@ -54,9 +48,9 @@
           data        = options.data      || null,
           async       = (typeof options.async == 'boolean') ? options.async : config.async,
           timeout     = options.timeout   || config.timeout,
-          success     = options.success   || nada,
+          success     = options.success   || _nada,
           error       = options.error     || config.error,
-          complete    = options.complete  || nada,
+          complete    = options.complete  || _nada,
           timeout_handler, user_input;
           
       xhr.onreadystatechange = function(){
@@ -136,10 +130,6 @@
     _destroy = function(path, options) {
       return _request('DELETE', _buildUrl(path), _parseOptions(options));
     },
-    
-    // http://mite.yo.lk/en/api/account.html
-    account             = function(options)              { return    _get('account',                            options); },
-    myself              = function(options)              { return    _get('myself',                             options); },
 
     Base = {
       _name             : function()                     { return this._url.replace(/s$/, "").replace(/ie$/, "y"); },
@@ -161,85 +151,51 @@
       create            : undefined,
       update            : undefined,
       destroy           : undefined
-    },
+    };
 
-    // http://mite.yo.lk/en/api/time-entries.html
-    // see also: http://mite.yo.lk/en/api/grouped-time-entries.html
-    TimeEntry = _extend({
-      _url      : 'time_entries'
-    }, Base),
-    
-    // http://mite.yo.lk/en/api/tracker.html
-    Tracker = {
-      find              : function(options)              { return    _get('tracker',                                options); },
-      start             : function(id, options)          { return    _put('tracker/'+id,                        {}, options); },
-      stop              : function(id, options)          { return    _destroy('tracker/'+id,                        options); }
-    },
-    
-    // http://mite.yo.lk/en/api/bookmarks.html
-    Bookmark = _extend({
-      _url              : 'time_entries/bookmarks',
-      // TODO fix me (I guess it relates to the redirect)
-      time_entries_for  : function(id, options)          { return    _get(this._url + '/' + id + '/follow',   options); }
-    }, Base, OnlyReadable),
-    
-        
-    // http://mite.yo.lk/en/api/customers.html
-    Customer = _extend({
-      _url              : 'customers',
-      projects_for      : function(ids, options)         { return    _get('projects?customer_id='+ids,              options); },
-      time_entries_for  : function(ids, options)         { return    _get('time_entries?customer_id='+ids,          options); }
-    }, ActiveArchivedBase),
-    
-    // http://mite.yo.lk/en/api/projects.html
-    Project = _extend({
-      _url              : 'projects',
-      time_entries_for  : function(ids, options)         { return    _get('time_entries?project_id='+ids,           options); }
-    }, ActiveArchivedBase),
-    
-    // http://mite.yo.lk/en/api/services.html
-    Service = _extend({
-      _url              : 'services',
-      time_entries_for  : function(ids, options)         { return    _get('time_entries?service_id='+ids,           options); }
-    }, ActiveArchivedBase),
-    
-    // http://mite.yo.lk/en/api/users.html
-    User = _extend({
-      _url              : 'users',
-      time_entries_for  : function(ids, options)         { return    _get('time_entries?user_id='+ids,              options); }
-    }, ActiveArchivedBase, OnlyReadable),
-    
     //// 
     //  Public
-    Interface = function(options) {
-      if (!options || !options.account || !options.api_key) {
-        throw "account & api_key need to be set";
-      }
-      
-      config.protocol = options.protocol || defaults.protocol;
-      config.domain   = options.domain   || defaults.domain;
-      config.account  = options.account  || defaults.account;
-      config.api_key  = options.api_key  || defaults.api_key;
-      config.async    = options.async    || defaults.async;
-      config.timeout  = options.timeout  || defaults.timeout;
-      config.error    = options.error    || defaults.error;
-      
-      this.config     = config;
+    return {
+      // http://mite.yo.lk/en/api/account.html
+      account   : function(options)              { return    _get('account',                            options); },
+      myself    : function(options)              { return    _get('myself',                             options); },
+      // http://mite.yo.lk/en/api/time-entries.html & http://mite.yo.lk/en/api/grouped-time-entries.html
+      TimeEntry : _extend({
+        _url      : 'time_entries'
+      }, Base),
+      // http://mite.yo.lk/en/api/tracker.html
+      Tracker   : {
+        find              : function(options)              { return    _get('tracker',                                options); },
+        start             : function(id, options)          { return    _put('tracker/'+id,                        {}, options); },
+        stop              : function(id, options)          { return    _destroy('tracker/'+id,                        options); }
+      },
+      // http://mite.yo.lk/en/api/bookmarks.html
+      Bookmark  : Bookmark = _extend({
+        _url              : 'time_entries/bookmarks',
+        // TODO fix me (I guess it relates to the redirect)
+        time_entries_for  : function(id, options)          { return    _get(this._url + '/' + id + '/follow',   options); }
+      }, Base, OnlyReadable),
+      // http://mite.yo.lk/en/api/customers.html
+      Customer  : _extend({
+        _url              : 'customers',
+        projects_for      : function(ids, options)         { return    _get('projects?customer_id='+ids,              options); },
+        time_entries_for  : function(ids, options)         { return    _get('time_entries?customer_id='+ids,          options); }
+      }, ActiveArchivedBase),
+      // http://mite.yo.lk/en/api/projects.html
+      Project   : _extend({
+        _url              : 'projects',
+        time_entries_for  : function(ids, options)         { return    _get('time_entries?project_id='+ids,           options); }
+      }, ActiveArchivedBase),
+      // http://mite.yo.lk/en/api/services.html
+      Service   : _extend({
+        _url              : 'services',
+        time_entries_for  : function(ids, options)         { return    _get('time_entries?service_id='+ids,           options); }
+      }, ActiveArchivedBase),
+      // http://mite.yo.lk/en/api/users.html
+      User      : _extend({
+        _url              : 'users',
+        time_entries_for  : function(ids, options)         { return    _get('time_entries?user_id='+ids,              options); }
+      }, ActiveArchivedBase, OnlyReadable)
     };
-    
-    Interface.prototype = {
-      // resources
-      account   : account,
-      myself    : myself,
-      TimeEntry : TimeEntry,
-      Tracker   : Tracker,
-      Bookmark  : Bookmark,
-      Customer  : Customer,
-      Project   : Project,
-      Service   : Service,
-      User      : User
-    };
-    
-    return Interface;
-  }());
+  };
 }(window));
